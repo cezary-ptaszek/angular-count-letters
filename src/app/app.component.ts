78eb941213eb85d142d344df9a0bcaee53f62708
cezary-ptaszek <cezaryptaszek@gmail.com>
2020-04-29 15:57:03 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'count-letters';
+
+  stringToCount: string;
+  temp: string;
+  listOfSum: Array<string> = [];
+
+  countLetters(){
+    this.listOfSum = [];
+    if (this.stringToCount !== undefined){
+      this.temp = this.stringToCount;
+      const numUpper = (this.temp.length - this.temp.replace(/[A-Z]/g, '').length).toString();
+      const numLower = (this.stringToCount.length - this.stringToCount.replace(/[a-z]/g, '').length).toString();
+      this.listOfSum.push('Given string: ' + this.stringToCount);
+      this.listOfSum.push('Big letters: ' + numUpper);
+      this.listOfSum.push('Small letters: ' + numLower);
+      this.stringToCount = undefined;
+    }
+  }
 }
